05c666c266470e8ee9fe878b380e301c7f7ba560
pratham-sarankar <pratham@sarankar.com>
2026-03-31 09:46:48 +0530

    Update user details service.

--- src/main/java/com/example/day11/service/CustomUserDetailsService.java
+++ src/main/java/com/example/day11/service/CustomUserDetailsService.java
@@ -1,18 +1,17 @@
 package com.example.day11.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
     @Override
     public UserDetails loadUserByUsername(String username) {
-
-        return org.springframework.security.core.userdetails.User.builder()
+        return User.builder()
                 .username(username)
-                .password("{noop}password")
+                .password("password")
                 .roles("USER")
                 .build();
     }
