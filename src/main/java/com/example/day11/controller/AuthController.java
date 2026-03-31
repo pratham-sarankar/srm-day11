a8aa206083755cb55029fce346d779ad0310a167
pratham-sarankar <pratham@sarankar.com>
2026-03-31 15:32:02 +0530

    Add register API

--- src/main/java/com/example/day11/controller/AuthController.java
+++ src/main/java/com/example/day11/controller/AuthController.java
@@ -1,18 +1,35 @@
 package com.example.day11.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.day11.dto.UserCreateRequest;
+import com.example.day11.model.User;
+import com.example.day11.service.UserService;
 import com.example.day11.util.JwtService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private JwtService jwtService;
 
-    @PostMapping("/auth/login")
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody UserCreateRequest request) {
+        User user = service.createUser(request.getEmail(), request.getPassword());
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/login")
     public String login(@RequestParam String username,
             @RequestParam String password) {
 
